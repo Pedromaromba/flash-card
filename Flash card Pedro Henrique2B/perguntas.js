e390ed5b015df40fbdc27f7d561ec7d06268a6eb
pedro <00001099403777sp@al.educacao.sp.gov.br>
2024-11-05 08:43:01 -0300

    Add files via upload
    flash card

---
+++ Flash card Pedro Henrique2B/perguntas.js
@@ -0,0 +1,56 @@
+criaCartao(
+    'matematica',
+    'Quem provou que 1:1 é igual a 2?',
+    ' Alfred Whitehead e Bertrand Russell'
+)
+
+criaCartao(
+    'fisica',
+    'O que é fisica?',
+    ' é a área das ciências naturais que estuda que acontecem com a matéria.'
+
+)
+
+criaCartao(
+    'matematica',
+    'para que serve a matemática?',
+    ' A matemática nos auxilia no raciocínio, inclusive, em outras disciplinas'
+)
+
+criaCartao(
+    'Educação fisica',
+    'quanto de água tomar por dia',
+    '35 ml diários para cada quilo que temos. '
+)
+
+criaCartao(
+    'Educação fisica',
+    'quantas calorias o cérebro gasta?',
+    'aproximadamente 260 calorias por dia'
+
+)
+
+criaCartao(
+    'Educação fisica',
+    'como calcular o gasto calorico?',
+    'DE = METs (da atividade) x Peso Corporal (Kg) x Duração (min/60).'
+)
+
+criaCartao(
+    'Química',
+    'O que significa mol na química?',
+    'A unidade de mol se refere ao número de moléculas, íons e átomos'
+)
+
+criaCartao(
+    'Química',
+    'Qual e o conceito de polaridade?',
+    ' capacidade que as ligações possuem de atrair cargas elétricas'
+
+)
+
+criaCartao(
+    'Química',
+    'O que e química?',
+    'é uma área da ciência natural '
+)
